fdb1af604bfaf8073fce2540a1c77a9180b8ccf4
Arjun T <33921467+unjar@users.noreply.github.com>
2017-12-15 01:32:37 -0800

    Added landing fragment, fixed issue with launch screen drawable for api 26+

--- app/src/main/java/me/unjar/beehold/MainActivity.java
+++ app/src/main/java/me/unjar/beehold/MainActivity.java
@@ -1,5 +1,6 @@
 package me.unjar.beehold;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LandingFragment landingFragment = new LandingFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.fragment_container, landingFragment)
+                .commit();
+
     }
 }
